0a94188168cec9973ed0766d9bf62cebcfd59427
spsbeish@gmail.com <spsbeish@gmail.com>
2023-04-25 23:31:20 +0800

    為字加上顏色

--- carbon-footpoint-calculator.cpp
+++ carbon-footpoint-calculator.cpp
@@ -2,8 +2,11 @@
 #include<iostream>
 #include"carbon_calculate.h"
 #include"clear.h"
+#include<string>
 
 using namespace std;
+
+
 
 int main()
 {   
@@ -14,56 +17,57 @@ int main()
     carbon_calculate Ccal;
     clear clear;
 
-    cout << "carbon footpoint calculator" << endl << endl;
-    cout << "welcome to the carbon foootpoint calculator"<<endl;
+    cout << "\x1B[33mcarbon footpoint calculator\x1b[0m" << endl << endl;
+    cout << "\x1B[31mwelcome to the carbon foootpoint calculator\x1b[0m"<<endl;
     system("pause");
     clear.Clear();
 
     while (true) {
         CarbonSum = 0;
         clear.Clear();
-        cout   << "carbon footpoint calculator" << endl << endl;
+        cout   << "\x1B[33mcarbon footpoint calculator\x1b[0m" << endl << endl;
 
 
-        cout << "一個月用水量(度):\n";
+        cout << "\x1B[36m一個月用水量(度):";
         cin >> Ccal.TapWater_mmm;
         CarbonSum += Ccal.Carbon_TaiwanTapWater(Ccal.TapWater_mmm);
         clear.Clear();
 
-        cout << "carbon footpoint calculator" << endl << endl;
-        cout << "一個月用電量(度):\n";
+        cout << "\x1B[33mcarbon footpoint calculator\x1b[0m" << endl << endl;
+        cout << "\x1B[36m一個月用電量(度):";
         cin >> Ccal.Eletricity_kwh;
         CarbonSum += Ccal.Carbon_TaiwanEletricity(Ccal.Eletricity_kwh);
         clear.Clear();
 
-        cout << "carbon footpoint calculator" << endl << endl;
-        cout << "家用小客車通勤人數:\n";
+        cout << "\x1B[33mcarbon footpoint calculator\x1b[0m" << endl << endl;
+        cout << "\x1B[36m家用小客車通勤人數:";
         cin >> Ccal.PrivateCar_person;
         CarbonSum += Ccal.Carbon_PrivateCar(Ccal.PrivateCar_person);
         clear.Clear();
 
-        cout << "carbon footpoint calculator" << endl << endl;
-        cout << "機車通勤人數:\n";
+        cout << "\x1B[33mcarbon footpoint calculator\x1b[0m" << endl << endl;
+        cout << "\x1B[36m機車通勤人數:";
         cin >> Ccal.MortorBike_person;
         CarbonSum += Ccal.Carbon_Mortorbike(Ccal.MortorBike_person);
         clear.Clear();
 
-        cout << "carbon footpoint calculator" << endl << endl;
-        cout << "校車路線總里程數:\n";
+        cout << "\x1B[33mcarbon footpoint calculator\x1b[0m" << endl << endl;
+        cout << "\x1B[36m校車路線總里程數:";
         cin >> Ccal.Bus_km;
         CarbonSum += Ccal.Carbon_Bus(Ccal.Bus_km);
         clear.Clear();
 
-        cout << "carbon footpoint calculator" << endl << endl;
-        cout << "住宿人數:\n";
+        cout << "\x1B[33mcarbon footpoint calculator\x1b[0m" << endl << endl;
+        cout << "\x1B[36m住宿人數:";
         cin >> Ccal.Dormitory_person;
         CarbonSum += Ccal.Carbon_dormitory(Ccal.Dormitory_person);
         clear.Clear();
 
-        cout << "carbon footpoint calculator" << endl << endl;
-        cout << "總碳足跡是:" << CarbonSum << "Kg" << endl<<endl;
+        
+        cout << "\x1B[33mcarbon footpoint calculator\x1b[0m" << endl << endl;
+        cout << "\x1B[32m總碳足跡是:" << CarbonSum << "Kg\x1b[0m" << endl << endl;
 
-        cout << "是否繼續(y/n)?";
+        cout << "\x1B[31m是否繼續(y/n)?\x1b[0m";
         cin >> ifagain;
 
         if (ifagain == 'y') {
